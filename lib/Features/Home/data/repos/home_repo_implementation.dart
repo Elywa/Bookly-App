368eafef0edc8097a0160a8c5cf07bb0984bb47c
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2024-08-19 17:08:21 +0100

    create Api Services class

---
+++ lib/Features/Home/data/repos/home_repo_implementation.dart
@@ -0,0 +1,19 @@
+import 'package:bookly_app/Core/errors/failure.dart';
+import 'package:bookly_app/Features/Home/data/book_model/book_model.dart';
+import 'package:bookly_app/Features/Home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImple implements HomeRepo {
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  } 
+
+}
